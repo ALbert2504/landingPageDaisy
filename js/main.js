020464929aca1c24b85212c9a1489b57455b4481
Aliko <61713118+ALbert2504@users.noreply.github.com>
2020-05-25 18:40:28 +0400

    first js

---
+++ js/main.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/************************ Section 8,, portfolio ****************************/
+
+// images equal heights
+const imgGift = document.querySelector('.gallery__image2 > img');
+const imgWebpage = document.querySelector('.gallery__image3');
+
+let imgWebpageHeight = parseFloat(getComputedStyle(imgWebpage).height);
+imgGift.style.height = `${imgWebpageHeight}px`;
+
+const imgCars = document.querySelector('.gallery__image6 > img');
+imgCars.style.height = `${imgWebpageHeight}px`;
+
+const imgBike = document.querySelector('.gallery__image1');
+let imgBikeHeight = parseFloat(getComputedStyle(imgBike).height);
+const imgColors = document.querySelector('.gallery__image4 > img');
+const imgBus = document.querySelector('.gallery__image5 > img');
+imgColors.style.height = `${imgBikeHeight}px`;
+imgBus.style.height = `${imgBikeHeight}px`;
